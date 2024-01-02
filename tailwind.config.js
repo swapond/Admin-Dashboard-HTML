e0c1fe3b5bd0c8833b1c9db0857b9ccf64ef5b9f
Swapon Das <swapond@Swapons-MacBook-Air.local>
2024-01-02 19:18:08 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+    },
+    colors: {
+      blue: "#287bff",
+      white: "#fff",
+      gray: "#f5f5f5",
+      black1: "#222",
+      black2: "#999",
+    },
+  },
+  plugins: [],
+};
